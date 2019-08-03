444b3fe2380d73438063d762415715c06f211064
Paul Fowler <43384818+Xeneye@users.noreply.github.com>
2019-08-03 15:35:22 +0100

    Update 06-apps.sh

--- 06-apps.sh
+++ 06-apps.sh
@@ -12,7 +12,7 @@
 #                                           \______/                               
 #Install Favourite Apps
 echo "Installing Favourite Apps"
-sudo dnf -y install google-chrome-stable firefox telegram-desktop discord thunderbird gimp gimp-help-en_GB obs-studio audacity autokey-gtk ckb-next filezilla nano cmatrix neofetch blender vim vlc kdenlive frei0r-plugins krita amarok mlocate
+sudo dnf -y install google-chrome-stable firefox telegram-desktop discord thunderbird gimp gimp-help-en_GB obs-studio audacity autokey-gtk ckb-next filezilla nano cmatrix neofetch blender vim vlc kdenlive frei0r-plugins krita amarok mlocate kcolorchooser digikam akregator filelight 
 sleep 1.5
 sudo dnf -y update
 clear
