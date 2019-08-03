acffb796c98d1a7b56a37ffe0b668fb46d39c251
Paul Fowler <43384818+Xeneye@users.noreply.github.com>
2019-08-03 13:14:19 +0100

    Update 06-apps.sh

--- 06-apps.sh
+++ 06-apps.sh
@@ -12,7 +12,7 @@
 #                                           \______/                               
 #Install Favourite Apps
 echo "Installing Favourite Apps"
-sudo dnf -y install google-chrome-stable firefox telegram-desktop discord thunderbird gimp gimp-help-en_GB obs-studio audacity autokey-qt ckb-next filezilla fish nano cmatrix neofetch blender vim
+sudo dnf -y install google-chrome-stable firefox telegram-desktop discord thunderbird gimp gimp-help-en_GB obs-studio audacity autokey-gtk ckb-next filezilla fish nano cmatrix neofetch blender vim
 sleep 1.5
 sudo dnf -y update
 clear
