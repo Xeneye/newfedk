08930c6b2dd5340affb9ecd2bfac150014ea5779
Paul Fowler <43384818+Xeneye@users.noreply.github.com>
2019-08-03 11:27:09 +0100

    Update 04-nvidia.sh

--- 04-nvidia.sh
+++ 04-nvidia.sh
@@ -15,7 +15,8 @@ echo "Installing NVIDIA Drivers, Media Codecs & Essential Extras"
 sudo dnf -y update && sudo dnf -y install rpmfusion-free-release-tainted && sudo dnf -y install libdvdcss && sudo dnf -y update && sudo dnf -y groupupdate core sound-and-video multimedia && sudo dnf -y install curl libcurl nano util-linux-user cabextract xorg-x11-drv-nvidia xorg-x11-drv-nvidia-cuda xorg-x11-drv-nvidia-cuda-libs akmod-nvidia vulkan vdpauinfo libva-vdpau-driver libva-utils libnfs-utils alsa-plugins-pulseaudio && sudo dnf -y install gstreamer-plugins-bad gstreamer-plugins-ugly lame faad2 && sudo dnf -y install ffmpeg ffmpeg-libs gstreamer-ffmpeg && sudo dnf -y install libaacs libbdplus gstreamer1-libav.x86_64 bash-completion kernel-modules-extra pulseaudio pulseaudio-utils pavucontrol alsa-plugins-pulseaudio
 wget https://developer.download.nvidia.com/compute/cuda/repos/fedora29/x86_64/cuda-repo-fedora29-10.1.168-1.x86_64.rpm
 sudo rpm -i cuda-repo-*.rpm
-sudo dnf -y install cuda-drivers cuda-libraries-10.0
+sudo dnf -y install cuda
+sudo rm cuda-repo-*.rpm
 sleep 1.5
 sudo dnf -y update
 clear
