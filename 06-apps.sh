4b2d0081d2a0a2fe34682d342452e1abe452474e
Paul Fowler <43384818+Xeneye@users.noreply.github.com>
2019-08-03 12:02:11 +0100

    Create 06-apps.sh

---
+++ 06-apps.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+#   $$\   $$\                                                 
+#   $$ |  $$ |                                                 
+#   \$$\ $$  |$$$$$$\  $$$$$$$\   $$$$$$\  $$\   $$\  $$$$$$\  
+#    \$$$$  /$$  __$$\ $$  __$$\ $$  __$$\ $$ |  $$ |$$  __$$\ 
+#    $$  $$< $$$$$$$$ |$$ |  $$ |$$$$$$$$ |$$ |  $$ |$$$$$$$$ |
+#   $$  /\$$\$$   ____|$$ |  $$ |$$   ____|$$ |  $$ |$$   ____|
+#   $$ /  $$ \$$$$$$$\ $$ |  $$ |\$$$$$$$\ \$$$$$$$ |\$$$$$$$\ 
+#   \__|  \__|\_______|\__|  \__| \_______| \____$$ | \_______|
+#                                          $$\   $$ |  
+#                                          \$$$$$$  |                              
+#                                           \______/                               
+#Install Favourite Apps
+echo "Installing Favourite Apps"
+sudo dnf -y install google-chrome-stable telegram-desktop discord thunderbird gimp gimp-help-en_GB obs-studio audacity autokey-qt ckb-next filezilla fish nano cmatrix neofetch blender vim
+sleep 1.5
+sudo dnf -y update
+clear
